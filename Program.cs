e58fa0f72dfa7a00897929bd12958e49f2c203e6
Jim Stanley <jim.stanley9@gmail.com>
2020-05-03 07:36:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetVS2019Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
